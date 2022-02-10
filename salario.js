b25e4a260869febb8a16a84fd10ab903ab96b905
usuario <pablo_tiu@hotmail.com>
2022-02-10 16:22:06 -0300

    Añadir el index.html con resto de html

---
+++ salario.js
@@ -0,0 +1,43 @@
+const colombia = [];
+
+colombia.push(
+    {
+        name:"Pablo",
+        salary: 1500
+    }
+);
+
+colombia.push(
+    {
+        name:"Natalia",
+        salary: 1000
+    }
+);
+
+colombia.push(
+    {
+        name:"Luis",
+        salary: 1800
+    }
+);
+
+colombia.push(
+    {
+        name:"Lucas",
+        salary: 1200
+    }
+);
+
+colombia.push(
+    {
+        name:"Benja",
+        salary: 5800
+    }
+);
+
+colombia.push(
+    {
+        name:"Maria Jose",
+        salary: 2800
+    }
+);
---
+++ salarioArgentina.js
@@ -0,0 +1,43 @@
+const salario = [];
+
+salario.push(
+    {
+    name:"Pablo",
+    salary:80000
+    }
+);
+
+salario.push(
+    {
+    name:"Lucas",
+    salary:50000
+    }
+);
+
+salario.push(
+    {
+    name:"Pedro",
+    salary:80000
+    }
+);
+
+salario.push(
+    {
+    name:"Lorena",
+    salary:120000
+    }
+);
+
+salario.push(
+    {
+    name:"Ricardo",
+    salary:20000
+    }
+);
+
+salario.push(
+    {
+    name:"Lucio",
+    salary:5000
+    }
+);
